92b0154c5605be6c5b2107f1e229a299da7e4a74
kritika12345kumari <150222251+kritika12345kumari@users.noreply.github.com>
2024-03-14 12:12:04 +0530

    Add files via upload

---
+++ kritika.js
@@ -0,0 +1,11 @@
+//date=[ 23,34,56,6543]
+  //  document.write(date[1])
+
+//Number=[23,45,56,67,78,56]
+//document.write(Number.length)
+
+Number=[34,56,67,35,345,234,3456,890]
+for(i=0;Number;length++i)
+    if(Number[i]%2==0){
+        document.write(Number[i],"<br>")
+    }
